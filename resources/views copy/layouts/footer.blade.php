f422417bacf98834bbd4ecd79e17d43f17b91524
Syahril ardi <aeeeeeng@gmail.com>
2022-01-14 15:02:59 +0700

    [NEW TEMPLATE] - remove old public & old view

--- resources/views copy/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 1.2
-    </div>
-    <strong>Copyright &copy; {{ '2021' }} <a href="/">{{ $setting->nama_perusahaan }}</a>.</strong> All rights
-    reserved.
-</footer>
