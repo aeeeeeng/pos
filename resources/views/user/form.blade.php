918e56cde1e971c0345dc8ee6af0bf4a87b2072e
Syahril ardi <aeeeeeng@gmail.com>
2022-01-17 11:10:09 +0700

    [NEW TEMPLATE] - user

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -6,26 +6,25 @@
 
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title"></h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="form-group row">
+                    <div class="form-group mb-2 row">
                         <label for="name" class="col-lg-3 col-lg-offset-1 control-label">Nama</label>
                         <div class="col-lg-6">
                             <input type="text" name="name" id="name" class="form-control" required autofocus>
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group mb-2 row">
                         <label for="email" class="col-lg-3 col-lg-offset-1 control-label">Email</label>
                         <div class="col-lg-6">
                             <input type="email" name="email" id="email" class="form-control" required>
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group mb-2 row">
                         <label for="password" class="col-lg-3 col-lg-offset-1 control-label">Password</label>
                         <div class="col-lg-6">
                             <input type="password" name="password" id="password" class="form-control" 
@@ -34,7 +33,7 @@
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
-                    <div class="form-group row">
+                    <div class="form-group mb-2 row">
                         <label for="password_confirmation" class="col-lg-3 col-lg-offset-1 control-label">Konfirmasi Password</label>
                         <div class="col-lg-6">
                             <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" 
@@ -46,7 +45,7 @@
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-sm btn-flat btn-primary"><i class="fa fa-save"></i> Simpan</button>
-                    <button type="button" class="btn btn-sm btn-flat btn-warning" data-dismiss="modal"><i class="fa fa-arrow-circle-left"></i> Batal</button>
+                    <button type="button" class="btn btn-sm btn-flat btn-warning" data-bs-dismiss="modal"><i class="fa fa-arrow-circle-left"></i> Batal</button>
                 </div>
             </div>
         </form>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,25 +6,31 @@
 
 @section('breadcrumb')
     @parent
-    <li class="active">Daftar User</li>
+    <li class="breadcrumb-item active">Daftar User</li>
 @endsection
 
 @section('content')
 <div class="row">
     <div class="col-lg-12">
-        <div class="box">
-            <div class="box-header with-border">
-                <button onclick="addForm('{{ route('user.store') }}')" class="btn btn-success btn-xs btn-flat"><i class="fa fa-plus-circle"></i> Tambah</button>
+        <div class="card">
+            <div class="card-header with-border">
+                <button onclick="addForm('{{ route('user.store') }}')" class="btn btn-success btn-sm btn-flat"><i class="fa fa-plus-circle"></i> Tambah</button>
             </div>
-            <div class="box-body table-responsive">
-                <table class="table table-stiped table-bordered">
-                    <thead>
-                        <th width="5%">No</th>
-                        <th>Nama</th>
-                        <th>Email</th>
-                        <th width="15%"><i class="fa fa-cog"></i></th>
-                    </thead>
-                </table>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="table-responsive">
+                            <table class="table table-sm table-stiped table-bordered">
+                                <thead>
+                                    <th width="5%">No</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th><i class="fa fa-cog"></i></th>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
